ad5d971c006a10a0e3d37d181806e5215a251ebe
carlaclefig <carlaclefig@gmail.com>
2024-12-03 14:38:03 -0500

    feat: codigo de account, funcionabilidades de creación de cuenta, realizar deposito y retiro, eliminación,obtención y listar cuentas

---
+++ src/main/java/com/xyzbank/AccountMs/controller/AccountController.java
@@ -0,0 +1,57 @@
+package com.xyzbank.AccountMs.controller;
+
+import com.xyzbank.AccountMs.model.Account;
+import com.xyzbank.AccountMs.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getAccountById(@PathVariable Long id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PutMapping("/{accountId}/deposit")
+    public ResponseEntity<Object> deposit(@PathVariable Long accountId, @RequestBody AmountRequest amountRequest) {
+        return accountService.deposit(accountId, amountRequest.getAmount());
+    }
+
+    @PutMapping("/{accountId}/withdraw")
+    public ResponseEntity<Object> withdraw(@PathVariable Long accountId, @RequestBody AmountRequest amountRequest) {
+        return accountService.withdraw(accountId, amountRequest.getAmount());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteAccount(@PathVariable Long id) {
+        return accountService.deleteAccount(id);
+    }
+
+    public static class AmountRequest {
+        private Double amount;
+
+        public Double getAmount() {
+            return amount;
+        }
+    }
+}
